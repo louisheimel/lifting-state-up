8bc27d0042ffeebc6b0e7533d2ba3050b4241259
Louis Heimel <louisheimel1@gmail.com>
2018-11-16 11:09:24 -0600

    solution

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Blue extends Component {
+class Small extends Component {
   state = {counter: 0};
-  incrementSmallCounter = () =>
+  incrementSmallCounter = () => {
+    this.props.handleSmallClick();
     this.setState({counter: this.state.counter + 1});
+  };
   render() {
     return (
-      <div className="blue" onClick={this.incrementSmallCounter}>
+      <div className="small" onClick={this.incrementSmallCounter}>
         <p>{this.state.counter}</p>
       </div>
     );
   }
 }
 
-class Red extends Component {
+class Big extends Component {
   state = {counter: 0};
   incrementBigCounter = e => {
     e.preventDefault();
@@ -23,19 +25,23 @@ class Red extends Component {
   };
   render() {
     return (
-      <div className="red" onClick={this.incrementBigCounter}>
-        <p>{this.state.counter}</p>
+      <div className="big" onClick={this.incrementBigCounter}>
+        <p>{this.state.counter + this.props.smallClicks}</p>
       </div>
     );
   }
 }
 
 class App extends Component {
+  state = {smallClicks: 0};
+  handleSmallClick = () =>
+    this.setState({smallClicks: this.state.smallClicks + 1});
+
   render() {
     return (
       <div className="App">
-        <Red />
-        <Blue />
+        <Big smallClicks={this.state.smallClicks} />
+        <Small handleSmallClick={this.handleSmallClick} />
       </div>
     );
   }
